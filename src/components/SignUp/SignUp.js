c8b446e0c6c43a1ecced8a9ab81b3013d5da9b35
--replace-all <ignaciobellomo@gmail.com>
2019-12-03 17:45:48 -0500

    form fixed

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -8,9 +8,8 @@ export default class Login extends Component {
             <div>
             <form action="http://192.168.125.251:3001/users" method="POST">
                 Username: <input type="text" name="username"/>  <br/>          
-                First Name: <input type="text" name="firstName" /><br/>
-                Last Name: <input type="text" name="lastName" /><br/>
-                {/* Image URL: <input type="text" name="image" /><br/> */}
+                First Name: <input type="text" name="first_name" /><br/>
+                Last Name: <input type="text" name="last_name" /><br/>
                 City: <input type="text" name="city" /><br/>
                 <button>Submit</button>
             </form>
