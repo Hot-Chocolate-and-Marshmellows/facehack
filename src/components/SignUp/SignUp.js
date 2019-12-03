26a7c8e7baf8aca37c514ddffa150a7164a59c62
--replace-all <ignaciobellomo@gmail.com>
2019-12-03 17:44:00 -0500

    apostrophe

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -6,7 +6,7 @@ export default class Login extends Component {
     render() {
         return (
             <div>
-            <form action="http://192.168.125.251:3001`/users" method="POST">
+            <form action="http://192.168.125.251:3001/users" method="POST">
                 Username: <input type="text" name="username"/>  <br/>          
                 First Name: <input type="text" name="firstName" /><br/>
                 Last Name: <input type="text" name="lastName" /><br/>
