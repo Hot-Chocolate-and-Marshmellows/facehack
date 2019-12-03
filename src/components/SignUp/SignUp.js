67e42297b81b7b7faf6ef1b181a4fb5a5534846b
avrahm <me@avrahm.com>
2019-12-03 17:41:30 -0500

    add user

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -6,11 +6,11 @@ export default class Login extends Component {
     render() {
         return (
             <div>
-            <form action="/list" method="POST" onSubmit={this.newUser}>
+            <form action="http://192.168.125.251:3001/users" method="POST">
                 Username: <input type="text" name="username"/>  <br/>          
                 First Name: <input type="text" name="firstName" /><br/>
                 Last Name: <input type="text" name="lastName" /><br/>
-                Image URL: <input type="text" name="image" /><br/>
+                {/* Image URL: <input type="text" name="image" /><br/> */}
                 City: <input type="text" name="city" /><br/>
                 <button>Submit</button>
             </form>
